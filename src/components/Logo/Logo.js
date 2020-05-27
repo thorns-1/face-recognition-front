0ae062d80217cec90360e7a1231a1e7bb3ae091c
IvanKryuchkov <55559380+IvanKryuchkov@users.noreply.github.com>
2020-05-27 23:32:14 +0300

    first commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <img className='br2 shadow-2' style={{display: 'flex', height:150, width:150}} src='http://storyo.ru/medal2/images/Medal006.png' alt='logo' />
+        </div>
+    );
+}
+export default Logo;
