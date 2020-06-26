62f99de334ce03a620a0c993062a853caea1de58
IvanKryuchkov <55559380+IvanKryuchkov@users.noreply.github.com>
2020-06-26 20:14:25 +0300

    design change

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,11 +5,11 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit}) => {
     return (
         <div>
             <p className='f3'>
-                {'Whith bourgeoisie technologies we now can detect humans and jews, enjoy!'}
+                {'With modern technologies we can now detect humans, enjoy!'}
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
+                <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} placeholder='Your image URL...' />
                 <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
                 onClick={onButtonSubmit} 
                 >Detect</button>
